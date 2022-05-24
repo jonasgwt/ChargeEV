96ea8328ec8f52f70eb582862f6969838ba4ce77
Jonas Goh <55698556+jonasgwt@users.noreply.github.com>
2022-05-25 00:29:39 +0800

    Added Host Page

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,59 +1,80 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { authentication } from "../firebase/firebase-config";
+import { Icon } from '@rneui/themed';
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
   updateUser,
-  updateCurrentUser
+  updateCurrentUser,
 } from "firebase/auth";
-import Welcome from './Welcome';
-import { Button } from 'react-native-elements';
+import Welcome from "./Welcome";
+import Host from "./Host.js"
+import { Button } from "react-native-elements";
 
-
-
-const NextScreen = ({ navigation }) => {
-
-
-  const SignOut = () => {
-    AsyncStorage.clear();
-    signOut(authentication)
+const Homepage = ({ navigation }) => {
+  const SignOut = async () => {
+    await AsyncStorage.clear();
+    await signOut(authentication);
     navigation.navigate("Welcome");
-  }
-
+  };
 
   return (
-    <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <SafeAreaView
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
       <Text>{authentication.currentUser.email}</Text>
       <Text>{authentication.currentUser.displayName}</Text>
       <Button title={"Sign Out"} onPress={SignOut}></Button>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const SettingsScreen = ({ navigation }) => {
   return (
-    <SafeAreaView style={{flex:1, alignItems:"center", justifyContent:"center"}}>
+    <SafeAreaView
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
       <Text>{authentication.currentUser.email}</Text>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-
-
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function HomeScreen() {
   return (
-      <Tab.Navigator>
-        <Tab.Screen name="Next Screen" component={NextScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#1BB530"
+      }}  
+    >
+      <Tab.Screen
+        name="Homepage"
+        component={Homepage}
+        options={{
+          tabBarLabel: (props) => (
+            <Text style={{ color: props.color }}>Home</Text>
+          ),
+          tabBarIcon: (props) => (
+            <Icon color={props.color} name='home' />
+          ),
+        }}
+      />
+      <Tab.Screen name="Host" component={Host} options={{
+          tabBarLabel: (props) => (
+            <Text style={{ color: props.color }}>Host</Text>
+          ),
+          tabBarIcon: (props) => (
+            <Icon color={props.color} name='group' />
+          ),
+        }}/>
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
   );
 }
-    
-
